492fc523b83bca4a6299285d7cbc163e312f883d
George Starcher <george@georgestarcher.com>
2017-03-18 15:25:14 -0500

    Update command to allow fields as args instead of named argument

--- README.txt
+++ README.txt
@@ -1,6 +1,7 @@
 Streaming Custom Search Command
- ... | esreplacefields fieldnames="rule_title,rule_description"
+ ... | esreplacefields rule_title rule_description
+
+Place the fields to have token replacement after the command esreplacefields separated by spaces.
 
 If a chosen field has tokens that are not in the event that field will be left unmodified.
-
 
--- bin/esreplacefields.py
+++ bin/esreplacefields.py
@@ -6,15 +6,13 @@ from splunklib.searchcommands import dispatch, StreamingCommand, Configuration, 
 @Configuration()
 class ESReplaceFieldsCommand(StreamingCommand):
 
-    fieldnames = Option(
-        doc='''
-        **Syntax:** **fieldnames=***<fieldnamelist>*
-        **Description:** Quoted Comma Delim List of fieldnames to do replacement on''',  require=True)
-
     def stream(self,events):
 
+        if len(self.fieldnames) < 1:
+            raise Exception('Please specify at least one field')
+
         for event in events:
-            for fieldname in self.fieldnames.split(","):
+            for fieldname in self.fieldnames:
                 fieldname = fieldname.strip()
                 if fieldname not in event: continue
                 tokenized_field = event.get(fieldname)
